c9d6d5df002136073b68396e6ea8c62b1f309652
Riazul Islam <riazul5207@gmail.com>
2024-08-28 14:20:46 +0600

    stack component updated

--- src/theme/components/Stack.tsx
+++ src/theme/components/Stack.tsx
@@ -2,7 +2,10 @@ import { Theme } from '@mui/material';
 import { Components } from '@mui/material/styles/components';
 
 const Stack: Components<Omit<Theme, 'components'>>['MuiStack'] = {
-  defaultProps: { useFlexGap: true },
+  defaultProps: {
+    useFlexGap: true,
+    direction: 'row',
+  },
 };
 
 export default Stack;
