d8ae01b2d19c4ec6b43342d65a8495e29b661732
Riazul Islam <riazul5207@gmail.com>
2024-09-02 16:54:13 +0600

    balance card updated

--- src/components/sections/dashboard/analytics/Balance.tsx
+++ src/components/sections/dashboard/analytics/Balance.tsx
@@ -1,27 +1,28 @@
-import { Stack, Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import IconifyIcon from 'components/base/IconifyIcon';
 
 const Balance = () => {
   return (
     <Stack
-      direction="row"
+      component={Paper}
       alignItems="center"
       justifyContent="space-between"
-      columnGap={2.25}
-      sx={{ bgcolor: 'common.white', borderRadius: 5, p: 2.5, minWidth: 200 }}
+      p={2.5}
+      height={100}
     >
       <div>
-        <Typography variant="body1" color="text.primary" noWrap>
+        <Typography variant="body2" color="text.disabled">
           Your Balance
         </Typography>
-        <Typography variant="h3" color="text.secondary">
+        <Typography mt={0.25} variant="h3">
           $1,000
         </Typography>
       </div>
-
       <Stack direction="row" alignItems="center" spacing={0.5}>
-        <IconifyIcon icon="circle-flags:um" sx={{ fontSize: 50 }} />
-        <IconifyIcon icon="ri:arrow-down-s-line" sx={{ fontSize: 24 }} />
+        <IconifyIcon icon="circle-flags:um" fontSize={50} />
+        <IconifyIcon icon="ri:arrow-down-s-line" color="text.disabled" fontSize={24} />
       </Stack>
     </Stack>
   );
