539a53b185716167a6603604969b6a0b0eab01c1
Riazul Islam <riazul5207@gmail.com>
2024-09-10 15:20:09 +0600

    dahsboard layout updated

---
+++ src/components/sections/dashboard/trending-nfts/index.tsx
@@ -0,0 +1,11 @@
+import { Paper, Typography } from '@mui/material';
+
+const TrendingNFTs = () => {
+  return (
+    <Paper sx={{ height: 390 }}>
+      <Typography variant="h6">Trending NFTs</Typography>
+    </Paper>
+  );
+};
+
+export default TrendingNFTs;
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,16 +1,17 @@
-import { Grid } from '@mui/material';
-import Analytics from 'components/sections/dashboard/analytics';
-import BusinessDesign from 'components/sections/dashboard/business-design';
+import Grid from '@mui/material/Grid';
 import Calendar from 'components/sections/dashboard/calendar';
+import Analytics from 'components/sections/dashboard/analytics';
+import TotalSpent from 'components/sections/dashboard/total-spent';
 import CardSecurity from 'components/sections/dashboard/card-security';
 import ComplexTable from 'components/sections/dashboard/complex-table';
-import History from 'components/sections/dashboard/history';
 import PiChart from 'components/sections/dashboard/your-pi-chart';
+import History from 'components/sections/dashboard/history';
 import Revenue from 'components/sections/dashboard/revenue';
 import Tasks from 'components/sections/dashboard/tasks';
 import TeamMembers from 'components/sections/dashboard/team-members';
-import TotalSpent from 'components/sections/dashboard/total-spent';
 import DailyTraffic from 'components/sections/dashboard/daily-traffic';
+import TrendingNFTs from 'components/sections/dashboard/trending-nfts';
+import BusinessDesign from 'components/sections/dashboard/business-design';
 
 const Dashboard = () => {
   return (
@@ -24,36 +25,35 @@ const Dashboard = () => {
       <Grid item xs={12} lg={6}>
         <Revenue />
       </Grid>
-      <Grid item xs={12} lg={4}>
+      <Grid item xs={12} lg={3}>
+        <CardSecurity />
+      </Grid>
+      <Grid item xs={12} lg={3}>
+        <Tasks />
+      </Grid>
+      <Grid item xs={12} lg={3}>
+        <DailyTraffic />
+      </Grid>
+      <Grid item xs={12} lg={3}>
+        <PiChart />
+      </Grid>
+      <Grid item xs={6}>
+        <TrendingNFTs />
+      </Grid>
+      <Grid item xs={12} lg={3}>
+        <History />
+      </Grid>
+      <Grid item xs={12} lg={3}>
+        <Calendar />
+      </Grid>
+      <Grid item xs={12} lg={3}>
         <BusinessDesign />
+      </Grid>
+      <Grid item xs={12} lg={3}>
+        <TeamMembers />
       </Grid>
       <Grid item xs={12} lg={6}>
         <ComplexTable />
-      </Grid>
-
-      <Grid item xs={12} lg={4}>
-        <CardSecurity />
-      </Grid>
-      <Grid item xs={12} lg={6}>
-        <History />
-      </Grid>
-      <Grid item xs={12} lg={4}>
-        <TeamMembers />
-      </Grid>
-      <Grid item xs={12} lg={4}>
-        <Tasks />
-      </Grid>
-      <Grid item xs={12} lg={4}>
-        <Calendar />
-      </Grid>
-      <Grid item xs={12} lg={4}>
-        <PiChart />
-      </Grid>
-      <Grid item xs={12} lg={4}>
-        <DailyTraffic />
-      </Grid>
-      <Grid item xs={12}>
-        Starbucks
       </Grid>
     </Grid>
   );
