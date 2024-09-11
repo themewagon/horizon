8ba7f84c828f9ebb2643887319e32ffd7037d252
Riazul Islam <riazul5207@gmail.com>
2024-09-11 11:58:37 +0600

    customized react swiper

--- src/components/sections/dashboard/trending-nfts/index.tsx
+++ src/components/sections/dashboard/trending-nfts/index.tsx
@@ -2,7 +2,7 @@ import { Paper, Typography } from '@mui/material';
 
 const TrendingNFTs = () => {
   return (
-    <Paper sx={{ height: 390 }}>
+    <Paper sx={{ height: 350 }}>
       <Typography variant="h6">Trending NFTs</Typography>
     </Paper>
   );
