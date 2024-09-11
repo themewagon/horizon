5e25a6c3a520b9661b251152c5ee59fe8234741b
Riazul Islam <riazul5207@gmail.com>
2024-09-11 20:17:21 +0600

    business design card updated

--- src/components/sections/dashboard/business-design/index.tsx
+++ src/components/sections/dashboard/business-design/index.tsx
@@ -1,55 +1,62 @@
-import {
-  Box,
-  Paper,
-  Stack,
-  ButtonBase,
-  Typography,
-  Avatar,
-  AvatarGroup,
-  Button,
-} from '@mui/material';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+import AvatarGroup from '@mui/material/AvatarGroup';
 import IconifyIcon from 'components/base/IconifyIcon';
-import Avatar1 from 'assets/images/avatars/avatar1.png';
-import Avatar2 from 'assets/images/avatars/avatar2.png';
-import Avatar3 from 'assets/images/avatars/avatar3.png';
-import Avatar4 from 'assets/images/avatars/avatar4.png';
-import Avatar5 from 'assets/images/avatars/avatar5.png';
+
+import { Avatar1, Avatar2, Avatar3, Avatar4, Avatar5 } from 'data/images';
 
 const avatars = [Avatar1, Avatar2, Avatar3, Avatar4, Avatar5];
 
 const BusinessDesign = () => {
   return (
-    <Stack component={Paper} direction="column" p={0} height={390} overflow="hidden">
-      <Box p={3.5}>
-        <Stack spacing={1.5} alignItems="center">
-          <Stack
-            component={ButtonBase}
-            alignItems="center"
-            justifyContent="center"
-            height={52}
-            width={52}
-            bgcolor="error.light"
-            borderRadius={3}
-          >
-            <IconifyIcon icon="mdi:fire" color="error.main" fontSize="h2.fontSize" />
-          </Stack>
-
-          <Box mt={-0.5}>
-            <Typography variant="caption" color="text.disabled">
-              Business Design
-            </Typography>
-            <Typography mt={-0.35} variant="body1" fontWeight={700} noWrap>
-              New lession is available
-            </Typography>
-          </Box>
+    <Stack
+      component={Paper}
+      direction="column"
+      justifyContent="space-between"
+      overflow="hidden"
+      height={390}
+      p={0}
+    >
+      <Stack p={3.5} pb={1} spacing={1.5} alignItems="center">
+        <Stack
+          component={ButtonBase}
+          alignItems="center"
+          justifyContent="center"
+          height={52}
+          width={52}
+          bgcolor="error.light"
+          borderRadius={3}
+        >
+          <IconifyIcon icon="mdi:fire" color="error.main" fontSize="h2.fontSize" />
         </Stack>
 
-        <Typography mt={6} variant="h5" lineHeight={1.65} noWrap>
-          What do you need to know to <br /> create better products?
-        </Typography>
-      </Box>
+        <Box mt={-0.5}>
+          <Typography variant="caption" color="text.disabled">
+            Business Design
+          </Typography>
+          <Typography mt={-0.35} variant="body1" fontWeight={700} noWrap>
+            New lession is available
+          </Typography>
+        </Box>
+      </Stack>
 
-      <Stack direction="column" mt="auto" px={3.5} py={3} height={165} bgcolor="info.light">
+      <Typography px={3.5} variant="h5" lineHeight={1.65} noWrap>
+        What do you need to know to <br /> create better products?
+      </Typography>
+
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        px={3.5}
+        py={2.5}
+        height={150}
+        bgcolor="info.light"
+      >
         <Stack alignItems="center" spacing={3.5}>
           <Stack alignItems="center" justifyContent="center" borderRadius={2.5} spacing={0.75}>
             <IconifyIcon icon="ic:sharp-timer" color="success.dark" fontSize="h3.fontSize" />
@@ -70,7 +77,7 @@ const BusinessDesign = () => {
           </Stack>
         </Stack>
 
-        <Stack mt="auto" alignItems="center" justifyContent="space-between">
+        <Stack alignItems="center" justifyContent="space-between">
           <Stack height={1} alignItems="center">
             <AvatarGroup
               renderSurplus={(surplus) => (
@@ -79,6 +86,7 @@ const BusinessDesign = () => {
                 </Typography>
               )}
               total={22}
+              sx={{ '& .MuiAvatarGroup-avatar': { height: 32, width: 32 } }}
             >
               {avatars.map((avatar: string, index) => (
                 <Avatar key={avatar} alt="avatar" src={avatar} sx={{ zIndex: index }} />
